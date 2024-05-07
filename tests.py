d61cb9e90f09efd0fb5e621eed11783de520a41f
mrnotcrazy <thedarkunkown@gmail.com>
2024-05-07 01:35:40 -0400

    Update tests.py
    updated to include list villagers

--- tests.py
+++ tests.py
@@ -1,19 +1,19 @@
 import requests
 
-# Base URL of the Flask server
-BASE_URL = 'http://127.0.0.1:5000'
+
+API_URL = 'http://127.0.0.1:5000'
 
 
 def test_new_village():
 	print("Testing /new_village route...")
-	response = requests.post(f'{BASE_URL}/new_village')
+	response = requests.post(f'{API_URL}/new_village')
 	village_data = response.json()
 	print("Response:", village_data)
 
 
 def test_new_villager():
 	print("Testing /new_villager route")
-	response = requests.post(f'{BASE_URL}/new_villager')
+	response = requests.post(f'{API_URL}/new_villager')
 	villager_data = response.json()
 	print("Response:", villager_data)
 	return villager_data.get('villager_id')
@@ -26,7 +26,7 @@ def test_villager_mod(villager_id):
 		"variable": "home_village",
 		"value": "New Village"
 	}
-	response = requests.post(f'{BASE_URL}/villager_mod', json=data)
+	response = requests.post(f'{API_URL}/villager_mod', json=data)
 	print("Response:", response.json())
 
 
@@ -36,7 +36,7 @@ def test_add_event(villager_id):
 		"villager_id": villager_id,
 		"event": "Moved to new house"
 	}
-	response = requests.post(f'{BASE_URL}/add_event', json=data)
+	response = requests.post(f'{API_URL}/add_event', json=data)
 	print("Response:", response.json())
 
 
@@ -45,7 +45,13 @@ def test_undo_event(villager_id):
 	data = {
 		"villager_id": villager_id
 	}
-	response = requests.post(f'{BASE_URL}/undo_event', json=data)
+	response = requests.post(f'{API_URL}/undo_event', json=data)
+	print("Response:", response.json())
+
+
+def test_list_villagers():
+	print("Testing /villagers route to list all villagers")
+	response = requests.get(f'{API_URL}/villagers')
 	print("Response:", response.json())
 
 
@@ -54,4 +60,6 @@ if __name__ == "__main__":
 	village_id = test_new_villager()
 	test_villager_mod(village_id)  # Use the village ID for testing villager modification
 	test_add_event(village_id)  # Use the village ID for testing adding events
+	test_list_villagers()
 	test_undo_event(village_id)  # Use the village ID for testing undoing events
+	test_list_villagers()
